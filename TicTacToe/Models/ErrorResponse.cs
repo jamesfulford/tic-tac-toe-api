9c662a052eb2b3b001cdd8d91897d5552cd8a70f
James Fulford <james.fulford@outlook.com>
2019-02-13 23:45:37 -0500

    docs(core): Adding param details for error code

--- TicTacToe/Models/ErrorResponse.cs
+++ TicTacToe/Models/ErrorResponse.cs
@@ -17,6 +17,7 @@ namespace TicTacToe.Models {
         /// Error Response constructor
         /// </summary>
         /// <param name="message">Text to send to requestor.</param>
+        /// <param name="code">Error code to send to requestor. 1 indicates an improper input, 0 is a catch-all.</param>
         public ErrorResponse (int code, string message) {
             this.code = code;
             this.message = message;
